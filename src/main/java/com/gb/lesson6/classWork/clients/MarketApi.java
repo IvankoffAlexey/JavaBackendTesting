b5be0c117b5813c6cb611d2820f7c9f35f0896b3
IvankoffAlexey <alexey.ivankoff@gmail.com>
2022-06-21 03:10:09 +0300

    Добавлена classWork к уроку 6

---
+++ src/main/java/com/gb/lesson6/classWork/clients/MarketApi.java
@@ -0,0 +1,20 @@
+package com.gb.lesson6.classWork.clients;
+
+import com.gb.lesson6.classWork.market.models.Product;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.*;
+
+import java.util.List;
+
+public interface MarketApi {
+
+    @GET("products")
+    Call<List<Product>> getProducts();
+
+    @GET("products/{id}")
+    Call<Product> getProduct(@Path("id") Long id);
+
+    @POST("products")
+    Call<Product> postProduct(@Body Product product);
+}

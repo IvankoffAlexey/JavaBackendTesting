ec70dd26d023424e82b0043accd58846573c096d
IvankoffAlexey <alexey.ivankoff@gmail.com>
2022-06-22 19:42:35 +0300

    Выполнено практическое задание к уроку 6.

--- src/main/java/com/gb/lesson6/classWork/clients/MarketService.java
+++ src/main/java/com/gb/lesson6/classWork/clients/MarketService.java
@@ -3,9 +3,12 @@ package com.gb.lesson6.classWork.clients;
 import com.gb.lesson6.classWork.market.models.Product;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Call;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+import java.io.IOException;
 import java.util.List;
 
 import static com.gb.lesson5.classWork.clients.RetrofitCallExecutor.executeCall;
@@ -16,7 +19,7 @@ public class MarketService {
 
     public MarketService(){
         HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor(System.out::println);
-        interceptor.setLevel(HttpLoggingInterceptor.Level.BASIC);
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
 
         OkHttpClient client = new OkHttpClient.Builder()
                 .addInterceptor(interceptor)
@@ -35,13 +38,21 @@ public class MarketService {
         return executeCall(api.getProducts());
     }
 
-    public Product getProduct(long id) {
-
-        return executeCall(api.getProduct(id));
+    public Response<Product> getProductById(Long id) throws IOException {
+        Call<Product> call = api.getProductById(id);
+        return call.execute();
     }
 
     public Product createProduct(Product product) {
 
-     return executeCall(api.postProduct(product));
+     return executeCall(api.createProduct(product));
+    }
+
+    public Product updateProduct(Product product) {
+        return executeCall(api.updateProduct(product));
+    }
+    public Response<Void> deleteProduct(Long id) throws IOException {
+        Call<Void> call = api.deleteProduct(id);
+        return call.execute();
     }
 }

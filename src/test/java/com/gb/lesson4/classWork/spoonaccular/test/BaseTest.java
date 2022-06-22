76176fc77201d716b1b3c63891a61a8c5c42a8a8
IvankoffAlexey <alexey.ivankoff@gmail.com>
2022-06-22 20:19:20 +0300

    Выполнено практическое задание к уроку 4.

--- src/test/java/com/gb/lesson4/classWork/spoonaccular/test/BaseTest.java
+++ src/test/java/com/gb/lesson4/classWork/spoonaccular/test/BaseTest.java
@@ -1,12 +1,14 @@
 package com.gb.lesson4.classWork.spoonaccular.test;
 
 import com.gb.lesson4.classWork.spoonaccular.AbstractTest;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 
 public class BaseTest extends AbstractTest {
     @Test
+    @Disabled
     void test() throws IOException {
         System.out.println(getResource("/text.txt"));
     }
